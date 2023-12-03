ec6e6017d230223542cf4ca7f2123b7f62091ea3
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2023-12-03 22:52:49 +0530

    Update Basics.py

--- Basics.py
+++ Basics.py
@@ -18,6 +18,6 @@ while True:
         cv2.polylines(img,[pts],True,(255,0,255),5)
         pts2 = barcode.rect
         cv2.putText(img,myData,(pts2[0],pts2[1]),cv2.FONT_HERSHEY_SIMPLEX, 0.9,(255,0,255),2)
-
+        
     cv2.imshow('Result',img)
     cv2.waitKey(1)
